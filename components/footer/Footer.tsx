7927dfcf4447b5af770dd4dd494db17a3fc23e5e
Ravi Teja <itsraviteja.r@gmail.com>
2023-08-30 03:06:06 +0530

    updated footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="border-t py-4">
+      <div className="container">
+        <p className="leading-7 text-muted-foreground">
+          Made by{" "}
+          <a
+            href="https://github.com/r4vit3j4"
+            target="_blank"
+            className="font-medium underline text-primary underline-offset-2"
+          >
+            Ravi Teja
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Navbar from "../navbar/Navbar";
 import { cn } from "@/lib/utils";
 import Head from "next/head";
+import Footer from "../footer/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -16,9 +17,12 @@ const Layout = ({ children }: LayoutProps) => {
         <title>Next Blog App</title>
         <meta name="description" content="Next Blog App Assignment" />
       </Head>
-      <div className={cn("relative", inter.className)}>
+      <div
+        className={cn("relative flex flex-col min-h-[100svh]", inter.className)}
+      >
         <Navbar />
-        <div className="mt-32 sm:mt-28 my-20">{children}</div>
+        <div className="flex-1 mt-32 sm:mt-28 mb-10">{children}</div>
+        <Footer />
       </div>
     </>
   );
